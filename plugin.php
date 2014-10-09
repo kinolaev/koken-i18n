110efdf4dc41b5d87a035e7e36cc3482eeb2a717
kinolaev <kinolaev@gmail.com>
2014-10-10 00:02:56 +0400

    version 0.3

--- plugin.php
+++ plugin.php
@@ -2,46 +2,63 @@
 
 class KokenI18n extends KokenPlugin {
 
+	private $lang = false;
+
 	function __construct()
 	{
 		$this->require_setup = true;
 		$this->register_filter('api.album', 'kokenI18nAlbum');
 		$this->register_filter('api.content', 'kokenI18nContent');
 		$this->register_filter('api.text', 'kokenI18nText');
-		$this->register_filter('api.truncate', 'kokenI18nTruncate');
-		$this->register_filter('site.template', 'kokenI18nLabels');
+		$this->register_filter('site.template', 'kokenI18nTemplate');
 		$this->register_filter('site.output', 'kokenI18nOutput');
 		$this->register_hook('before_closing_body', 'kokenI18nSwitcher');
 		$this->register_hook('before_closing_head', 'kokenI18nSwitcherStyle');
 	}
 
-	private function kokenI18nWrapper($fields, $data)
+	private function kokenI18nWrapper($fields, $data, $options)
 	{
 		$lang_count = count(explode($this->data->separator, $this->data->lang_string));
-		foreach ($fields as $field) {
+
+		if (isset($options['lang']) && !empty($options['lang']))
+			$this->lang = $options['lang'];
+
+		foreach ($fields as $field)
+		{
 			if (count(explode($this->data->separator, $data[$field])) === $lang_count)
-				if (!preg_match('/^\[\[.*?\]\]$/s', $data[$field]))
-					$data[$field] = '[['.$data[$field].']]';
+			{
+				if (substr($data[$field], 0, 2) !== '[[')
+					$data[$field] = '[['.$data[$field];
+
+				if (substr($data[$field], -2, 2) !== ']]')
+					$data[$field] = $data[$field].']]';
+
+				if (!$options['auth'] && $this->lang)
+					$data[$field] = $this->kokenI18nExpand($data[$field]);
+			}
 		}
+
 		return $data;
 	}
 
 	private function kokenI18nCookie($setcookie = false)
 	{
-		$cookie = isset($_COOKIE['koken_i18n']) ? $_COOKIE['koken_i18n'] : $this->data->lang_default;
+		if ($this->lang === false)
+			$this->lang = isset($_COOKIE['koken_i18n']) ? $_COOKIE['koken_i18n'] : $this->data->lang_default;
 
-		if($setcookie)
-			setcookie('koken_i18n', $cookie, 0, '/');
+		if ($setcookie)
+			setcookie('koken_i18n', $this->lang, 0, '/');
 
-		return $cookie;
+		return $this->lang;
 	}
 
 	private function kokenI18nCallback($matches)
 	{
 		$lang = explode($this->data->separator, $this->data->lang_string);
 		$text = explode($this->data->separator, $matches[1]);
-		
-		if (count($lang) === count($text)) {
+
+		if (count($lang) === count($text))
+		{
 			$text = array_combine($lang, $text);
 			$cookie = $this->kokenI18nCookie();
 
@@ -60,43 +77,42 @@ class KokenI18n extends KokenPlugin {
 		return preg_replace_callback('/\[\[((?:.(?!\[\[))+?)\]\]/s', array($this, 'kokenI18nCallback'), $html);
 	}
 
-	function kokenI18nAlbum($data)
+	function kokenI18nAlbum($data, $obj, $options)
 	{
-		return $this->kokenI18nWrapper(array('title', 'summary', 'description'), $data);
+		return $this->kokenI18nWrapper(array('title', 'summary', 'description'), $data, $options);
 	}
 
-	function kokenI18nContent($data)
+	function kokenI18nContent($data, $obj, $options)
 	{
-		return $this->kokenI18nWrapper(array('title', 'caption'), $data);
+		return $this->kokenI18nWrapper(array('title', 'caption'), $data, $options);
 	}
 
-	function kokenI18nText($data)
+	function kokenI18nText($data, $obj, $options)
 	{
-		return $this->kokenI18nWrapper(array('title', 'excerpt', 'content'), $data);
+		return $this->kokenI18nWrapper(array('title', 'excerpt', 'content'), $data, $options);
 	}
 
-	function kokenI18nLabels($tmpl)
+	function kokenI18nTemplate($tmpl)
 	{
+		Koken::$location['lang'] = Koken::$location['parameters']['__overrides']['lang'] = $this->kokenI18nCookie();
+
 		$nums = array('singular', 'plural');
-		foreach (Koken::$site['url_data'] as $key => $value) {
-			if (is_array($value)) {
-
+		foreach (Koken::$site['url_data'] as $key => $value)
+		{
+			if (is_array($value))
+			{
 				foreach ($nums as $num)
 					if (isset($this->data->{'labels_'.$key.'_'.$num}))
-						Koken::$site['url_data'][$key][$num] = '[['.$this->data->{'labels_'.$key.'_'.$num}.']]';
-
-			} else if (is_string($value)) {
-
+						Koken::$site['url_data'][$key][$num] = $this->kokenI18nExpand('[['.$this->data->{'labels_'.$key.'_'.$num}.']]');
+			}
+			else if (is_string($value))
+			{
 				if (isset($this->data->{'labels_'.$key}))
-					Koken::$site['url_data'][$key] = '[['.$this->data->{'labels_'.$key}.']]';
-
+					Koken::$site['url_data'][$key] = $this->kokenI18nExpand('[['.$this->data->{'labels_'.$key}.']]');
 			}
 		}
-		return $tmpl;
-	}
 
-	function kokenI18nTruncate($str) {
-		return $this->kokenI18nExpand($str);
+		return $tmpl;
 	}
 
 	function kokenI18nOutput($html)
@@ -109,12 +125,15 @@ class KokenI18n extends KokenPlugin {
 
 	function kokenI18nSwitcher()
 	{
-		if($this->data->default_switcher) {
+		if ($this->data->default_switcher)
+		{
 			$lang = explode($this->data->separator, $this->data->lang_string);
 			$li = '';
-			foreach($lang as $l) {
+			foreach($lang as $l)
+			{
 				$li .= '<li><a href="#" data-lang="'.$l.'">'.$l.'</a></li>';
 			}
+
 			echo <<<OUT
 <ul id="lang-switcher">{$li}</ul>
 <script>
@@ -133,7 +152,8 @@ OUT;
 
 	function kokenI18nSwitcherStyle()
 	{
-		if($this->data->default_switcher_style) {
+		if ($this->data->default_switcher_style)
+		{
 			echo <<<OUT
 <style>
 #lang-switcher {
